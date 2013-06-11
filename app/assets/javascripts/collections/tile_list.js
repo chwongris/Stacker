a2c683c5c5fc36448ddf67d12f155edf30308526
Chris Wong <chwongris@gmail.com>
2013-06-11 16:43:47 -0400

    removed files

--- app/assets/javascripts/collections/tile_list.js
+++
@@ -1,5 +0,0 @@
-app.collections.TileList = Backbone.Collection.extend({
-
-  model: app.models.ParentTile
-  
-});
--- app/assets/javascripts/init.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-
-  var router = new app.Router();
-  Backbone.history.start({pushState: true});
-  router.navigate('');
-
-});

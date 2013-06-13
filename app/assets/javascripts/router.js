b1778e9606bc0516d4a20e48e1f4ec3feda742f6
Chris Wong <chwongris@gmail.com>
2013-06-13 15:07:22 -0400

    change applicationjs

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,12 +2,12 @@ app.Router = Backbone.Router.extend({
 
   routes: {
     '' : 'home',
-    '_=_' : 'home'
+    '_=_' : 'home',
+    'userstacks' : 'userstacks'
   },
 
   home: function () {
     //load the user and its saved restaurants
-
     var current_user = new app.models.User;
     current_user.url = 'users/me';
     current_user.fetch({
@@ -27,69 +27,73 @@ app.Router = Backbone.Router.extend({
         current_user.stack_list = new app.collections.StackList();
 
 
-        current_user.RestTileList.fetch({
-          success: function(){
-           var start = new app.views.StartView({ model: current_user });
-           $('#content').html(start.render().el);
-           initMap();
-           $('.datepicker').datepicker();
-          
-           var myDate = new Date();
-           var prettyDate =(myDate.getMonth()+1) + '/' + myDate.getDate() + '/' +
-           myDate.getFullYear();
-           $(".datepicker").val(prettyDate);
+          current_user.RestTileList.fetch({
+            success: function(){
+             var start = new app.views.StartView({ model: current_user });
+             $('#content').html(start.render().el);
+             initMap();
+             $('.datepicker').datepicker();
 
-           map.panBy(-200, 0);
+             var myDate = new Date();
+             var prettyDate =(myDate.getMonth()+1) + '/' + myDate.getDate() + '/' +
+             myDate.getFullYear();
+             $(".datepicker").val(prettyDate);
 
-           $('#calendar').fullCalendar({
-            header: {
-              left: 'prev,next today',
-              center: 'title',
-              right: 'agendaDay, agendaWeek',
-              ignoreTimezone: false
-            },
-            defaultView: 'agendaDay',
-            firstDay: 5,
-            firstHour: 18,
-            selectable: true,
-            selectHelper: true,
-            editable: true,
-            droppable: true,
-            height: 440,
-            drop: function(date, allDay) {
-                // alert("Dropped on " + date + " with allDay=" + allDay);
-                var dropped = current_user.rest_tile_search.get(this.id);
+             map.panBy(-200, 0);
 
-                // retrieve the dropped element's stored Event Object
-                var originalEventObject = $(this).data('eventObject');
+             $('#calendar').fullCalendar({
+              header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'agendaDay, agendaWeek',
+                ignoreTimezone: false
+              },
+              defaultView: 'agendaDay',
+              firstDay: 5,
+              firstHour: 18,
+              selectable: true,
+              selectHelper: true,
+              editable: true,
+              droppable: true,
+              height: 440,
+                drop: function(date, allDay) {
+                  // alert("Dropped on " + date + " with allDay=" + allDay);
+                  var dropped = current_user.rest_tile_search.get(this.id);
 
-                // we need to copy it, so that multiple events don't have a reference to the same object
-                var copiedEventObject = $.extend({}, originalEventObject);
-                
-                // assign it the date that was reported
-                if (copiedEventObject.start == undefined ){
-                  copiedEventObject.start = date;
-                  copiedEventObject.end = (date.getTime() + 1800000*4)/1000;
-                }else{
-                  copiedEventObject.start = copiedEventObject.start + "Z";
-                  var time = $.fullCalendar.parseDate( copiedEventObject.start );
-                  copiedEventObject.start = time;
-                  copiedEventObject.end = (time.getTime() + 1800000*4)/1000;
+                  // retrieve the dropped element's stored Event Object
+                  var originalEventObject = $(this).data('eventObject');
+
+                  // we need to copy it, so that multiple events don't have a reference to the same object
+                  var copiedEventObject = $.extend({}, originalEventObject);
+                  
+                  // assign it the date that was reported
+                    if (copiedEventObject.start == undefined ){
+                      copiedEventObject.start = date;
+                      copiedEventObject.end = (date.getTime() + 1800000*4)/1000;
+                    }else{
+                      copiedEventObject.start = copiedEventObject.start + "Z";
+                      var time = $.fullCalendar.parseDate( copiedEventObject.start );
+                      copiedEventObject.start = time;
+                      copiedEventObject.end = (time.getTime() + 1800000*4)/1000;
+                    }
+                  
+                  copiedEventObject.allDay = allDay;
+                  copiedEventObject.cid = this.id;
+                  
+                  // render the event on the calendar
+                  // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+                  $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
                 }
-                
-                copiedEventObject.allDay = allDay;
-                copiedEventObject.cid = this.id;
-                
-                // render the event on the calendar
-                // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-                $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
-              }
-            });
-}
-});
-}
-});
-},
+              });
+            }
+          });
+        }
+      });
+  },
+
+  userstacks: function() {
+    
+  }
 
 
 });

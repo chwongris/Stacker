c8863eec578e215a37717c5494a393e08d3a17dd
Chris Wong <chwongris@gmail.com>
2013-06-14 15:14:39 -0400

    fix login

--- config/routes.rb
+++ config/routes.rb
@@ -72,7 +72,7 @@ Stacker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#index'
+  root :to => 'homepage#index'
 
   # authenticated do 
   #   root :to => 'users#index'

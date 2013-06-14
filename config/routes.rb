7cf486854c5f83ff1b7cba0f00d09773289b3b65
Chris Wong <chwongris@gmail.com>
2013-06-14 15:09:11 -0400

    fixed signin

--- config/routes.rb
+++ config/routes.rb
@@ -72,8 +72,11 @@ Stacker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'homepage#index'
+  root :to => 'users#index'
 
+  # authenticated do 
+  #   root :to => 'users#index'
+  # end
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
